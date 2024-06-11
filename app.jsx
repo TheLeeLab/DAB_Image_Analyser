b0ccbc3318cecb1df8921b6cf242e3f43a87d15b
Chigozie Nri <chigozie@gmail.com>
2024-06-11 22:33:55 +0100

    FileDropZone as modal floating above everything at start

--- app.jsx
+++ app.jsx
@@ -79,20 +79,24 @@ function FileDropZone({dabAnalysisImages, setDabAnalysisImages}) {
     };
 
     return (
-        <div
-            onDragOver={onDragOver}
-            onDrop={onDrop}
-            className="flex items-center justify-center w-full"
-        >
-            <div className="max-w-2xl mx-auto">
-                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                    <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                        <svg className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                    </label>
-                    {/* Note: File selector in HTML file input doesn't support choosing both files and directories from the same dialog.
-                    For now, drag and drop, or highlight multiple files if you want to upload a whole directory at once */}
-                    <input onChange={(evt) => {uploadFiles(evt.target.files)}} id="dropzone-file" type="file" className="hidden" multiple />
+        <div id="static-modal" data-modal-backdrop="static" tabIndex="-1" aria-hidden="true"
+        className={`${dabAnalysisImages.length > 0 ? 'hidden': ''} overflow-y-hidden overflow-x-hidden fixed top-0 right-0 bottom-0 left-0 z-50 flex h-screen justify-center items-center`
+        }>
+            <div
+                onDragOver={onDragOver}
+                onDrop={onDrop}
+                className="flex items-center justify-center w-full h-full min-w-[40rem]"
+            >
+                <div className="h-full w-full mx-[10rem] items-center justify-center">
+                    <div className="flex flex-col w-full h-full items-center justify-center">
+                        <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-full m-10 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                            <svg className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400 text-center"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                        </label>
+                        {/* Note: File selector in HTML file input doesn't support choosing both files and directories from the same dialog.
+                        For now, drag and drop, or highlight multiple files if you want to upload a whole directory at once */}
+                        <input onChange={(evt) => {uploadFiles(evt.target.files)}} id="dropzone-file" type="file" className="hidden" multiple />
+                    </div>
                 </div>
             </div>
         </div>
