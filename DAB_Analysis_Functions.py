5d4062ccb4ecfc25f5f02659c56ed4111fdf3ea2
jbeckwith <jsbeckwith@gmail.com>
2024-06-12 16:03:47 +0100

    Otsu's Method

--- DAB_Analysis_Functions.py
+++ DAB_Analysis_Functions.py
@@ -11,6 +11,11 @@ import numpy as np
 import skimage as ski
 import cv2
 import io
+from skimage.measure import label, regionprops_table
+from skimage.color import rgb2gray, hed2rgb, rgb2hed
+from skimage.morphology import reconstruction
+from skimage.filters import threshold_otsu
+import pandas as pd
 
 
 class DAB:
@@ -45,28 +50,6 @@ class DAB:
             img.astype(np.float32) / info.max
         )  # Divide all values by the largest possible value in the datatype
         return imdouble
-
-    def getMeanchannelvalues(self, image, mask):
-        """getMeanLABvalues function
-        takes image, image mask and gets mean channel components
-        assumes Lab colour space
-
-        Args:
-            image (np.2darray): numpy array
-            mask (np.2darray): numpy logical array
-
-        Returns:
-            imdouble is numpy array"""
-        LMean = np.nanmean(
-            np.multiply(image[:, :, 0], mask)
-        )  # mean of only the pixels within the masked area.
-        aMean = np.nanmean(
-            np.multiply(image[:, :, 1], mask)
-        )  # mean of only the pixels within the masked area.
-        bMean = np.nanmean(
-            np.multiply(image[:, :, 2], mask)
-        )  # mean of only the pixels within the masked area.
-        return LMean, aMean, bMean
 
     def pseudo_circularity(self, MajorAxisLength, MinorAxisLength):
         """pseudo_circularity function
@@ -105,8 +88,6 @@ class DAB:
             mask_filled, ski.morphology.disk(2)
         )
 
-        from skimage.measure import label, regionprops_table
-
         label_img = label(cleaned_mask)
         props = regionprops_table(label_img, properties=("area", "axis_minor_length"))
         Area = props["area"]
@@ -135,8 +116,6 @@ class DAB:
         mask_filled = ski.morphology.reconstruction(seed, mask_disk, method="erosion")
         cleaned_mask = ski.segmentation.clear_border(mask_filled)
 
-        from skimage.measure import label, regionprops_table
-
         label_img = label(cleaned_mask)
         props = regionprops_table(label_img, properties=("area", "axis_minor_length"))
         minorA = props["axis_minor_length"]
@@ -153,65 +132,25 @@ class DAB:
         cleaned_mask[image_mask] = 0
         return cleaned_mask
 
-    def colourFilterLab(
-        self, image, initial_params, rate=[0.75, 4], percentage=0.075, maxiter=30
-    ):
-        """colourFilterLab function
-        takes image, and uses initial parameters and rate to separate out
-        coloured objects
+    def otsu_filtering(self, image):
+        """otsu threshold a single colour image
 
         Args:
-            image (np.2darray): image input
-            initial_params (np.1darray): initial Lmean, aMean, bMean and threshold parameters
-            rate (np.float64): how fast to change the parameters per iteration
-            percentage (np.float64): percentage of pixels we worry about in the mask
-            maxiter (int): how many iterations of optimisation to do (default 30)
-
+            image (np.2darray): single grayscale image
         Returns:
-            image_mask (np.2darray) logical array of image mask
-            current_params (np.1darray) image analysis final params"""
-        mask_current = np.zeros(image[:, :, 0].shape)
-        LMean = initial_params[0]
-        aMean = initial_params[1]
-        bMean = initial_params[2]
-        thres = initial_params[3]
-        ratep_1 = rate[0]
-        ratep_2 = rate[1]
+            mask (np.2darray): single boolean mask image"""
 
-        iteration = 1
-        while (
-            np.nansum(mask_current) < np.multiply(percentage, image[:, :, 0].size)
-        ) and (iteration <= maxiter):
-            deltaL = np.subtract(image[:, :, 0], LMean)  # get mean channel 1
-            deltaa = np.subtract(image[:, :, 1], aMean)  # get mean channel 2
-            deltab = np.subtract(image[:, :, 2], bMean)  # get mean channel 3
-            deltaE = np.sqrt(
-                (deltaL**2 + deltaa**2 + deltab**2)
-            )  # get change versus iteration
-            mask_previous = mask_current  # store previous mask
-            mask_current = 1 * (deltaE <= thres)  # update mask
-            mask_current = np.where(mask_current == 0, np.nan, mask_current)
-            LMean, aMean, bMean = self.getMeanchannelvalues(
-                image, mask_current
-            )  # get new means
-            LMean = np.multiply(LMean, ratep_1)  # adjust means with rate of change
-            aMean = np.multiply(aMean, ratep_1)  # adjust means with rate of change
-            bMean = np.multiply(bMean, ratep_1)  # adjust means with rate of change
+        seed = np.copy(image)
+        seed[1:-1, 1:-1] = image.max()
+        mask = image
 
-            dEmask = np.multiply(deltaE, mask_current)
-            meanMaskedDeltaE = np.nanmean(dEmask)
-            stDevMaskedDeltaE = np.nanstd(dEmask)
-            thres = np.add(meanMaskedDeltaE, np.multiply(ratep_2, stDevMaskedDeltaE))
-            iteration += 1
+        filled = reconstruction(seed, mask, method="erosion")
+        holes = np.abs(image - filled)
+        thresh = threshold_otsu(holes)
+        mask = holes > thresh
+        return mask
 
-        image_mask = mask_previous  # update mask
-        image_mask[np.isnan(image_mask)] = 0
-        current_params = np.array([LMean, aMean, bMean, thres])
-        return image_mask, current_params
-
-    def analyse_DAB(
-        self, img, filename, asyn_params=np.array([27, 6, 5, 15]), use_defaults=1, check_mask=0
-    ):
+    def analyse_DAB(self, img, filename):
         """analyse_DAB function
         takes file, and uses initial parameters and rate to separate out
         coloured objects
@@ -227,103 +166,80 @@ class DAB:
             image_mask_asyn (np.2darray): mask of protein
             table_asyn (pd.DataArray): pandas array of asyn data
             asyn_params (pd.DataArray): parameters used to gets asyn mask"""
-        lab_Image = ski.color.rgb2lab(self.im2double(img))
-        if use_defaults == 0:
-            init_guess = self.get_guess(self, img, lab_Image)
-            asyn_params = np.hstack([init_guess], asyn_params[-1])
 
-        image_mask_asyn, asyn_params = self.colourFilterLab(lab_Image, asyn_params)
+        ihc_hed = rgb2hed(self.im2double(img))
+        # Create an RGB image for each of the stains
+        null = np.zeros_like(ihc_hed[:, :, 0])
+        ihc_d = hed2rgb(np.stack((null, null, ihc_hed[:, :, 2]), axis=-1))
+
+        dab_image = rgb2gray(ihc_d)
+
+        image_mask_asyn = self.otsu_filtering(dab_image)
         image_mask_asyn = self.clean_protein_mask(image_mask_asyn)
-        if check_mask == 1:
-            masks = image_mask_asyn
-            self.plot_masks(img, masks)
-        from skimage.measure import label, regionprops_table
 
         label_img_asyn = label(image_mask_asyn)
         props_asyn = regionprops_table(
             label_img_asyn,
             properties=("area", "centroid", "axis_major_length", "axis_minor_length"),
         )
-        import pandas as pd
 
         table_asyn = pd.DataFrame(props_asyn)
         table_asyn["pseudo_circularity"] = self.pseudo_circularity(
             props_asyn["axis_major_length"], props_asyn["axis_minor_length"]
         )
-        table_asyn["filename"] = np.full_like(props_asyn["axis_minor_length"], 
-                                              filename, dtype='object')
-        
-        asyn_cols = ["asyn_LMean", "asyn_aMean", "asyn_bMean", "asyn_threshold"]
-        asyn_params = pd.DataFrame([asyn_params], columns=asyn_cols)
-        asyn_params["filename"] = np.full_like(asyn_params["asyn_LMean"], 
-                                              filename, dtype='object')
+        table_asyn["filename"] = np.full_like(
+            props_asyn["axis_minor_length"], filename, dtype="object"
+        )
 
-        return image_mask_asyn, table_asyn, asyn_params
+        return image_mask_asyn, table_asyn
 
-    def analyse_DAB_and_cells(
-        self,
-        img,
-        filename,
-        asyn_params=np.array([27, 6, 5, 15]),
-        nuclei_params=np.array([70, 1, -5, 4]),
-        use_defaults=1,
-        check_mask=0,
-    ):
-        """analyse_DAB_and_cells function
+    def analyse_DAB_and_cells(self, img, filename):
+        """analyse_DAB function
         takes file, and uses initial parameters and rate to separate out
         coloured objects
         then returns table with object information
 
         Args:
             img (np.ndarray): image data
-            filename (string): filename string
-            asyn_params (np.1darry): initial default Lmean, aMean, bMean and threshold parameters
-            nuclei_params (np.1darray): initial default Lmean, aMean, bMean and threshold parameters
+            filename (str): filename string
 
         Returns:
             image_mask_asyn (np.2darray): mask of protein
-            image_mask_nuclei (np.2darray): mask of nuclei
             table_asyn (pd.DataArray): pandas array of asyn data
-            table_nuclei (pd.DataArray): pandas array of nuclei data
-            asyn_params (pd.DataArray): parameters used to gets asyn mask
-            nuclei_params (pd.DataArray): parameters used to get nuclear mask"""
-        lab_Image = ski.color.rgb2lab(self.im2double(img))
-        if use_defaults == 0:
-            init_guess = self.get_guess(self, img, lab_Image)
-            asyn_params = np.hstack([init_guess], asyn_params[-1])
-            init_guess_n = self.get_guess(
-                self,
-                img,
-                lab_Image,
-                "select area that is just nuclear staining; press enter when complete",
-            )
-            nuclei_params = np.hstack([init_guess_n], nuclei_params[-1])
+            image_mask_nuclei (np.2darray): mask of nuclei
+            table_nuclei (pd.DataArray): pandas array of nuclei data"""
 
-        image_mask_asyn, asyn_params = self.colourFilterLab(lab_Image, asyn_params)
+        ihc_hed = rgb2hed(self.im2double(img))
+        # Create an RGB image for each of the stains
+        null = np.zeros_like(ihc_hed[:, :, 0])
+        ihc_d = hed2rgb(np.stack((null, null, ihc_hed[:, :, 2]), axis=-1))
+        ihc_h = hed2rgb(np.stack((ihc_hed[:, :, 0], null, null), axis=-1))
+
+        dab_image = rgb2gray(ihc_d)
+
+        image_mask_asyn = self.otsu_filtering(dab_image)
         image_mask_asyn = self.clean_protein_mask(image_mask_asyn)
-        image_mask_nuclei, nucl_params = self.colourFilterLab(
-            lab_Image, nuclei_params, rate=[1, 2]
-        )
+
+        test = ihc_h
+        test[image_mask_asyn == 1] = np.median(ihc_h[:, :, 0])
+        nuclei_image = rgb2gray(test)
+
+        image_mask_nuclei = self.otsu_filtering(nuclei_image)
         image_mask_nuclei = self.clean_nuclear_mask(image_mask_nuclei)
-        if check_mask == 1:
-            masks = np.dstack([image_mask_asyn, image_mask_nuclei])
-            self.plot_masks(img, masks)
-        from skimage.measure import label, regionprops_table
 
         label_img_asyn = label(image_mask_asyn)
         props_asyn = regionprops_table(
             label_img_asyn,
             properties=("area", "centroid", "axis_major_length", "axis_minor_length"),
         )
-        import pandas as pd
 
         table_asyn = pd.DataFrame(props_asyn)
         table_asyn["pseudo_circularity"] = self.pseudo_circularity(
             props_asyn["axis_major_length"], props_asyn["axis_minor_length"]
         )
-        table_asyn["filename"] = np.full_like(props_asyn["axis_minor_length"], 
-                                              filename, dtype='object')
-
+        table_asyn["filename"] = np.full_like(
+            props_asyn["axis_minor_length"], filename, dtype="object"
+        )
 
         label_img_nucl = label(image_mask_nuclei)
         props_nuclei = regionprops_table(
@@ -334,48 +250,7 @@ class DAB:
         table_nuclei["pseudo_circularity"] = self.pseudo_circularity(
             props_nuclei["axis_major_length"], props_nuclei["axis_minor_length"]
         )
-        table_nuclei["filename"] = np.full_like(table_nuclei["axis_minor_length"], 
-                                              filename, dtype='object')
-
-
-        asyn_cols = ["asyn_LMean", "asyn_aMean", "asyn_bMean", "asyn_threshold"]
-        asyn_params = pd.DataFrame([asyn_params], columns=asyn_cols)
-        asyn_params["filename"] = np.full_like(asyn_params["asyn_LMean"], 
-                                              filename, dtype='object')
-
-        nuclei_cols = [
-            "nuclei_LMean",
-            "nuclei_aMean",
-            "nuclei_bMean",
-            "nuclei_threshold",
-        ]
-        nuclei_params = pd.DataFrame([nucl_params], columns=nuclei_cols)
-        nuclei_params["filename"] = np.full_like(nuclei_params["nuclei_LMean"], 
-                                              filename, dtype='object')
-
-        return (
-            image_mask_asyn,
-            image_mask_nuclei,
-            table_asyn,
-            table_nuclei,
-            asyn_params,
-            nuclei_params,
-        )
-
-    def get_guess(
-        self,
-        img,
-        lab_image,
-        message="select area that is just DAB-stained protein aggregate; press enter when complete",
-    ):
-        import cv2
-
-        r = cv2.selectROI(message, cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-
-        cv2.destroyAllWindows()
-        area = lab_image[int(r[1]) : int(r[1] + r[3]), int(r[0]) : int(r[0] + r[2])]
-        init_guess = np.mean(np.mean(area, axis=1), axis=0)
-        return init_guess
+        return image_mask_asyn, table_asyn, image_mask_nuclei, table_nuclei
 
     def plot_masks(self, img, masks=None):
         """plot_masks function
